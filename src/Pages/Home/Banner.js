ba5ca45e0b33422c41c7d75e484ab80cc8098ee9
ahadhasanjion <niallhoran3176@gmail.com>
2022-11-24 02:41:29 +0600

    banner

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import bg from '../../Assets/1920x800_bg1.jpg'
+import './Banner.css'
 
 const Banner = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='mt-0 py-60' style={{background: `url(${bg})`}}>
+            <div className="hero">
+                <div className="hero-content flex-col lg:flex-row">
+                  
+                    <div className='banner-text text-center'>
+                        <h1 className='lg:text-8xl font-bold'>Best Place To Buy</h1>
+                        <h2 className='lg:text-6xl font-bold'>Buy & Sell Your Vehicle</h2>
+                        <p className='lg:text-2xl font-normal'>FIND CAR. ADD TO CARR. FREE DELIVERY.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
