2b16d10720b1a2eb05552f5f688d5b123ff77ced
ahadhasanjion <niallhoran3176@gmail.com>
2022-11-23 23:09:43 +0600

    footer

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="footer footer-center p-10 text-primary-content">
+            <div>
+                <p className="font-bold text-3xl">
+                    CARBS. <br />Providing Best Car  since 2010
+                </p>
+                <p className='text-lg'>Copyright © 2022 - All right reserved</p>
+            </div>
+        </footer>
     );
 };
 
